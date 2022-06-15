5dbe0d1909535aa970e8385f2cbeab306a970f81
m.poettgen@gmx.net <82958985+poettas@users.noreply.github.com>
2022-06-15 11:21:01 +0200

    added the banner as component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Header() {
     return (
-        <header>
-            <div>
+        <header className="flex justify-between p-5 max-w-7xl mx-auto">
+            <div className="flex items-center space-x-5">
                 <Link href="/">
                     <img
                         src="https://links.papareact.com/yvf"
@@ -12,13 +12,20 @@ function Header() {
                         className="w-44 object-contain cursor-pointer"
                     />
                 </Link>
-                <div>
+                <div className="hidden md:inline-flex items-center space-x-5">
                     <h3>About</h3>
                     <h3>Contact</h3>
-                    <h3>Follow</h3>
+                    <h3 className="text-white bg-green-700 px-4 py-1 rounded-full">
+                        Follow
+                    </h3>
                 </div>
             </div>
-            <div></div>
+            <div className="flex items-center space-x-5 text-green-700">
+                <h3>Sign In</h3>
+                <h3 className="border px-4 py-1 rounded-full border-green-700">
+                    Get Started
+                </h3>
+            </div>
         </header>
     );
 }
